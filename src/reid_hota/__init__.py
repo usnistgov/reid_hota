e1c633b14c2d4e4724655850c11aa8f28c274503
Michael Majurski <michael.majurski@nist.gov>
2025-08-22 16:07:43 -0400

    cleanup of init

--- src/reid_hota/__init__.py
+++ src/reid_hota/__init__.py
@@ -7,21 +7,11 @@ A fast, parallel implementation of HOTA metrics for re-identification and tracki
 __version__ = "0.2.0"
 
 from .reid_hota import HOTAReIDEvaluator
-from .hota_data import HOTAData#, VideoFrameData, FrameExtractionInputData
-# from .hota_utils import merge_hota_data, jaccard_cost_matrices
-# from .cost_matrix import CostMatrixData
-# from .sparse_matrix import Sparse2DMatrix, Sparse1DMatrix
+from .hota_data import HOTAData
 from .config import HOTAConfig
 
 __all__ = [
     "HOTAReIDEvaluator",
     "HOTAData", 
-    # "CostMatrixData",
-    # "VideoFrameData",
-    # "merge_hota_data",
-    # "jaccard_cost_matrices",
-    # "Sparse2DMatrix",
-    # "Sparse1DMatrix",
-    # "FrameExtractionInputData",
     "HOTAConfig"
 ] 
